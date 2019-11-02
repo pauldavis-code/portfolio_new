f1139376b4960f54e22e8c5b8eb9627ce994475c
Paul Davis <pauldavis@pauls-mbp.gktw.org>
2019-11-02 12:02:53 -0400

    framework created - routing established

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Switch, Route, Link, Redirect} from "react-router-dom"
+
+// import for pages
+import Home from "./pages/home.js"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    // routing
+    <Router>
+      <Switch>
+        <Route path="/home">
+          <Home />
+        </Route>
+        <Route path="/">
+          <Redirect to="/home" />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
